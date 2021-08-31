cca1ba4a8bc905af093a41681b11db6cfffefb5e
Jeroen VAN WIER <jeroen.vanwier@uni.lu>
2021-08-31 14:41:07 +0200

    Initial canvas

---
+++ beziers.js
@@ -0,0 +1,12 @@
+function setup() {
+	canvas = document.getElementById("mainCanvas");
+	glContext = canvas.getContext("webgl");
+	
+	if (glContext === null) {
+		alert("WebGL not supported by browser");
+	}
+	
+	//Reset canvas to pure black
+	gl.clearColor(0.0, 0.0, 0.0, 1.0);
+	gl.clear(gl.COLOR_BUFFER_BIT);
+}
